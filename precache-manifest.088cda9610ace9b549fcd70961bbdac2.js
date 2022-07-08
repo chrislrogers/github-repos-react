d661be1bd13756d9d007197bf19c1b9c84dc748e
Chris Rogers <47615352+chrislrogers@users.noreply.github.com>
2022-07-08 17:24:47 +0100

    Updates

---
+++ precache-manifest.088cda9610ace9b549fcd70961bbdac2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fbb74a65ba4333e9ed56",
+    "url": "/github-repos-react/static/css/main.fd20e52b.chunk.css"
+  },
+  {
+    "revision": "fbb74a65ba4333e9ed56",
+    "url": "/github-repos-react/static/js/main.fbb74a65.chunk.js"
+  },
+  {
+    "revision": "24758a8b680d33a928be",
+    "url": "/github-repos-react/static/js/1.24758a8b.chunk.js"
+  },
+  {
+    "revision": "114868bb7ed3f5d2c5e8",
+    "url": "/github-repos-react/static/js/runtime~main.114868bb.js"
+  },
+  {
+    "revision": "dac03b8da764cec7fb91bf6eb228ed9f",
+    "url": "/github-repos-react/index.html"
+  }
+];
